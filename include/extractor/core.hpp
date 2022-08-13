8a183a395a2c3d5266ec9a148d29e81c29a92488
Roman Rakytiak <r.rakytiak@gmail.com>
2022-08-13 18:49:20 +0200

    Added core doc

--- include/extractor/core.hpp
+++ include/extractor/core.hpp
@@ -14,26 +14,49 @@ namespace extract
 
     class Getter;
 
+    /**
+     * @brief Variables which can be checked in Extractor
+     */
     enum class Extract
     {
+        // Checks for space, tabs and new lines
         whitespace,
+        // Checks only for spaces and tabs
         blank,
+        // 0-9
         digit,
+        // a-z and A-Z
         ascii_letter,
+        // a-z 
         ascii_small,
+        // A-Z
         ascii_capital,
+        // .,"'(){}/* etc.
         special,
+        // (char)0 - (char)31
         ascii_control,
         ascii,
+        // (char)0
         null
     };
 
+    /**
+     * @brief Checks if one of the present are in the string
+     */
     template <Extract Type>
     struct One;
 
+
+    /**
+     * @brief Checks if any of the present are in the string
+     */
     template <Extract Type, Extract...Types>
     struct Any;
 
+    /**
+     * @brief Simple implementation of string view for c++11 and c++14
+     * 
+     */
     struct View
     {
         const char*  first;
